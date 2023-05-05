672ed482516d522a84064027a46d9ec8f796e813
Nikorambus <nick-mod@mail.ru>
2023-05-05 20:17:45 +0300

    Реализация функциональности приложения

--- my_app.py
+++ my_app.py
@@ -27,3 +27,39 @@ class Delivery(db.Model):
 
 with app.app_context():
     db.create_all()
+
+
+@app.route("/", methods=["POST", "GET"])
+def index():
+    return render_template("index.html")
+
+
+@app.route("/tarif_data", methods=["POST"])
+def tarif():
+    bd = Tariff.query.order_by(Tariff.Id).all()
+    return render_template("tarif_data.html", bd=bd)
+
+
+@app.route("/delivery_form", methods=["POST"])
+def delivery_form():
+    return render_template("/delivery_form.html")
+
+
+@app.route("/delivery_data", methods=["POST"])
+def delivery_data():
+    id = request.form.get("id")
+    phone = request.form.get("phone")
+    if ((Delivery.query.filter_by(Id=int(id)).first()) and (Delivery.query.filter_by(Number_of_destination=phone).first())):#Если данные существуют
+        if (Delivery.query.filter_by(Id=int(id)).first()) == (Delivery.query.filter_by(Number_of_destination=phone).first()): #Если равны
+            item = Delivery.query.filter_by(Id=int(id)).first()
+            name = item.Number_of_destination
+            time = item.Time_of_destination
+            car = item.Number_of_car
+            return render_template("/delivery_data.html", name=name, time=time, car=car)
+        else:
+            return render_template("/fall.html")
+    else:
+        return render_template("/fall.html")
+
+
+app.run(host="0.0.0.0")
